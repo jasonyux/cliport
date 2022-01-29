f5cf870f6aa7536af45187a291311c9dd967d3d9
Xiao Yu <jasonyux17@gmail.com>
2022-01-29 17:51:53 -0500

    added distractors

--- cliport/tasks/place_obj_in_container.py
+++ cliport/tasks/place_obj_in_container.py
@@ -18,6 +18,17 @@ class PlaceObjInContainer(Task):
         self.lang_template = "place {color1} {object} into {color2} {container}"
         self.task_completed_desc = "done placing object in container."
 
+    def __generate_box(self, zone_size, template):
+        half = np.float32(zone_size) / 2
+        replace = {'DIM': zone_size, 'HALF': half}
+        return self.fill_template(template, replace)
+
+    def __generate_hexagon(self, obj_color, template):
+        fname = os.path.join(self.assets_root, 'kitting', '17.obj')
+        scale = [0.005, 0.005, 0.002]
+        replace = {'FNAME': (fname,), 'SCALE': scale, 'COLOR': utils.COLORS[obj_color]}
+        return self.fill_template(template, replace)
+
     def reset(self, env):
         super().reset(env)
         n_bowls = 1
@@ -31,8 +42,8 @@ class PlaceObjInContainer(Task):
             'bowl': 'bowl/bowl.urdf'
         }
 
-        # Add bowls.
-        zone_size = self.get_random_size(0.05, 0.3, 0.05, 0.3, 0.05, 0.05)
+        # Add container.
+        zone_size = self.get_random_size(0.15, 0.25, 0.15, 0.25, 0.05, 0.05)
         bowl_size = (0.1, 0.1, 0)
         bowl_poses = []
         container_color = np.random.choice(["green", "red"])
@@ -40,10 +51,8 @@ class PlaceObjInContainer(Task):
         container_urdf = containers[container]
         for _ in range(n_bowls):
             if container == 'box': 
+                container_urdf = self.__generate_box(zone_size, container_urdf)
                 container_pose = self.get_random_pose(env, zone_size)
-                half = np.float32(zone_size) / 2
-                replace = {'DIM': zone_size, 'HALF': half}
-                container_urdf = self.fill_template(container_urdf, replace)
             else:
                 container_pose = self.get_random_pose(env, bowl_size)
             
@@ -51,7 +60,7 @@ class PlaceObjInContainer(Task):
             p.changeVisualShape(container_id, -1, rgbaColor=utils.COLORS[container_color] + [1])
             bowl_poses.append(container_pose)
 
-        # Add blocks.
+        # Add block.
         obj_color = np.random.choice(["green", "red", "yellow"])
         obj = np.random.choice(list(objects.keys()))
         obj_urdf = objects[obj]
@@ -60,11 +69,8 @@ class PlaceObjInContainer(Task):
         for _ in range(n_blocks):
             if obj == 'hexagon':
                 size = (0.2, 0.2, 0.05)
+                obj_urdf = self.__generate_hexagon(obj_color, obj_urdf)
                 pose = self.get_random_pose(env, size)
-                fname = os.path.join(self.assets_root, 'kitting', '17.obj')
-                scale = [0.005, 0.005, 0.002]
-                replace = {'FNAME': (fname,), 'SCALE': scale, 'COLOR': utils.COLORS[obj_color]}
-                obj_urdf = self.fill_template(obj_urdf, replace)
             else:
                 pose = self.get_random_pose(env, block_size)
             block_id = env.add_object(obj_urdf, pose)
@@ -81,25 +87,38 @@ class PlaceObjInContainer(Task):
             color2=container_color, container=container)
         )
 
-        """ # Colors of distractor objects.
-        bowl_colors = [utils.COLORS[c] for c in utils.COLORS if c != 'green']
-        block_colors = [utils.COLORS[c] for c in utils.COLORS if c != 'blue']
+        # Colors of distractor objects.
+        container_colors = [c for c in utils.COLORS.keys() if c != container_color]
+        obj_colors = [c for c in utils.COLORS if c != obj_color]
 
         # Add distractors.
         n_distractors = 0
-        block_urdf = 'block/block.urdf'
-        bowl_urdf = 'bowl/bowl.urdf'
-        while n_distractors < 6:
-            is_block = np.random.rand() > 0.5
-            urdf = block_urdf if is_block else bowl_urdf
-            size = block_size if is_block else bowl_size
-            colors = block_colors if is_block else bowl_colors
-            pose = self.get_random_pose(env, size)
+        while n_distractors < 4:
+            distractor_class = np.random.choice([objects, containers])
+            distractor = np.random.choice(list(distractor_class.keys()))
+
+            colors = container_colors if distractor_class == containers else obj_colors
+            color = np.random.choice(colors)
+            urdf = distractor_class[distractor]
+
+            if distractor in objects.keys():
+                if distractor == 'hexagon':
+                    size = (0.2, 0.2, 0.05)
+                    urdf = self.__generate_hexagon(color, urdf)
+                    pose = self.get_random_pose(env, size)
+                else:
+                    pose = self.get_random_pose(env, block_size)
+            else:
+                if distractor == 'box': 
+                    urdf = self.__generate_box(zone_size, urdf)
+                    pose = self.get_random_pose(env, zone_size)
+                else:
+                    pose = self.get_random_pose(env, bowl_size)
+            
             if not pose:
                 continue
-            obj_id = env.add_object(urdf, pose)
-            color = colors[n_distractors % len(colors)]
-            if not obj_id:
+            id = env.add_object(urdf, pose)
+            if not id:
                 continue
-            p.changeVisualShape(obj_id, -1, rgbaColor=color + [1])
-            n_distractors += 1 """
+            p.changeVisualShape(id, -1, rgbaColor=utils.COLORS[color] + [1])
+            n_distractors += 1
